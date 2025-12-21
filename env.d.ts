9b36ff8ca4d1e8c1b1b8f78df408f9c0802aa190
Connor Abbas <abbasconnor@gmail.com>
2025-12-21 17:28:47 +0000

    changes to run build/preview

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,16 @@
 /// <reference types="vite/client" />
 declare module "envConfig";
+
+declare module 'primevue' {
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+    export * from 'primevue/index' // This helps resolve the index.js exports
+}
+
+// Allow sub-module imports from the src directory
+declare module 'primevue/*' {
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
